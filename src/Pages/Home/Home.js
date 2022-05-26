70b9d5add97f849348306001490eef9a8b6fb738
lamiamostafa <lamiamostafa791@gmail.com>
2022-05-26 09:56:37 +0600

    reviews are added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -2,10 +2,9 @@ import React from 'react';
 import Footer from '../Shared/Footer';
 import Banner from './Banner';
 import Products from './Products';
-// import Info from './Info';
-// import MakeAppointment from './MakeAppointment';
-// import Services from './Services';
-// import Testimonials from './Testimonials';
+import Reviews from './Reviews';
+
+
 
 
 const Home = () => {
@@ -13,10 +12,7 @@ const Home = () => {
         <div className="">
             <Banner></Banner>
             <Products></Products>
-            {/* <Info ></Info>
-            <Services></Services>
-            <MakeAppointment></MakeAppointment>
-            <Testimonials></Testimonials> */}
+            <Reviews></Reviews>
             <Footer></Footer>
 
         </div>
---
+++ src/Pages/Home/Reviews.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react';
+import quote from '../../assets/icons/quote.svg';
+import Review from './Review';
+
+const Reviews = () => {
+    const [reviews, setReviews] = useState([]);
+    useEffect(() => {
+        fetch('reviews.json')
+            .then(res => res.json())
+            .then(data => {
+                setReviews(data)
+            });
+
+    }, [])
+    return (
+        <section className="my-20">
+            <div className="flex justify-between">
+                <div>
+                    <h4 className="text-3xl text-[#06396C] font-bold">Reviews</h4>
+                    <h2 className="text-3xl">What Our Clients say</h2>
+                </div>
+                <div>
+                    <img src={quote} className=" w-24 lg:w-48" alt="" />
+                </div>
+            </div>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+                {
+                    reviews.map(review => <Review
+                        key={review._id}
+                        review={review}
+
+                    ></Review>)
+                }
+            </div>
+        </section>
+    );
+};
+
+export default Reviews;
