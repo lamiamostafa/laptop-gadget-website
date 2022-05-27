9f7395875be64fac880c41fd49ac02cc158de34f
lamiamostafa <lamiamostafa791@gmail.com>
2022-05-28 02:24:29 +0600

    admin verfication is added and require admin page is also added

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link, Outlet } from 'react-router-dom';
 import auth from '../../firebase.init';
+import useAdmin from '../../hooks/useAdmin';
 
 const Dashboard = () => {
     const [user] = useAuthState(auth);
-    // const [admin] = useAdmin(user);
+    const [admin] = useAdmin(user);
     return (
         <div class="drawer drawer-mobile">
             <input id="dashboard-sidebar" type="checkbox" class="drawer-toggle" />
@@ -19,13 +20,15 @@ const Dashboard = () => {
                     <li><Link to="/dashboard">My Order</Link></li>
                     <li><Link to="/dashboard/review">Add Reviews</Link></li>
                     <li><Link to="/dashboard/profile">My Profile</Link></li>
-                    <li><Link to="/dashboard/users">All Users</Link></li>
-                    
-                    {/* {admin && <>
+
+
+                    {admin && <>
                         <li><Link to="/dashboard/users">All Users</Link></li>
-                        <li><Link to="/dashboard/addDoctor">Add a Doctor</Link></li>
-                        <li><Link to="/dashboard/manageDoctor">Manage Doctors</Link></li>
-                    </>} */}
+                        <li><Link to="/dashboard/addproduct">Add Product</Link></li>
+                        <li><Link to="/dashboard/manageproduct">Manage Product</Link></li>
+                        <li><Link to="/dashboard/manageproduct">My Profile</Link></li>
+                        <li><Link to="/dashboard/manageallorder">Manage All Order</Link></li>
+                    </>}
                 </ul>
 
             </div>
